e97c3b31e2ac699dde3fc9a180ab295ce3ff632f
dam2t <zhiyang520679@gmail.com>
2024-02-22 21:12:24 +0100

    update

--- app/src/main/java/com/zy/proyecto_final/fragment/OrderFragment.kt
+++ app/src/main/java/com/zy/proyecto_final/fragment/OrderFragment.kt
@@ -1,6 +1,7 @@
 package com.zy.proyecto_final.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -28,7 +29,7 @@ class OrderFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_order, container, false)
-        recyclerView = view.findViewById(R.id.recyclerView)
+        recyclerView = view.findViewById(R.id.recyclerView_order)
         recyclerView.layoutManager =
             GridLayoutManager(context, 1)
         recyclerView.adapter =
@@ -38,13 +39,18 @@ class OrderFragment : Fragment() {
                 )
             }
 
+        loadData()
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_order, container, false)
     }
 
-    companion object {
-
-
+    private fun loadData() {
+        viewmodel.items.value?.let {
+            (view?.findViewById<RecyclerView>(R.id.recyclerView_order)!!.adapter as OrderRecyclerViewAdapter).setValues(
+                it.toMutableList()
+            )
+        }
     }
+
 
 }
--- app/src/main/java/com/zy/proyecto_final/recyclerviewadapter/OrderRecyclerViewAdapter.kt
+++ app/src/main/java/com/zy/proyecto_final/recyclerviewadapter/OrderRecyclerViewAdapter.kt
@@ -1,5 +1,6 @@
 package com.zy.proyecto_final.recyclerviewadapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
