247ae140a15a27c071e72fba3ef34410db5580ba
yang <zhiyang520679@gmail.com>
2024-05-09 13:10:02 +0200

    update

--- app/src/main/java/com/zy/proyecto_final/retrofit/entities/PaymentData.kt
+++ app/src/main/java/com/zy/proyecto_final/retrofit/entities/PaymentData.kt
@@ -4,6 +4,8 @@ import com.google.gson.annotations.SerializedName
 
 data class PaymentData(
     @SerializedName("type")     var type: Int = 0,
-    @SerializedName("amout")    var amout: Double = 0.0
-
+    @SerializedName("amout")    var amout: Double = 0.0,
+    @SerializedName("phone")    var phone: String = "",
+    @SerializedName("name")    var email: String = "",
+    @SerializedName("address")  var address: String = ""
     )
